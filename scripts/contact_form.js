42817379c3e10f3242653a7fa35539117fb17814
Qrofeus <ninad9021@gmail.com>
2023-04-28 21:25:55 +0530

    Revert to basics - Image illustrations added

--- scripts/contact_form.js
+++ scripts/contact_form.js
@@ -1,14 +1,15 @@
-function send_email() {
-    Email.send({
-        SecureToken : "846303c5-e9f9-4670-ad88-556873b12df2",
-        To : "ninad.deshpande46@gmail.com",
-        From : "qrofeus@gmail.com",
-        Subject : "New contact form enquiry",
-        Body : "Name: " + document.getElementById("form_name").value
-            +  "<br>Mail Id: " + document.getElementById("form_mail").value
-            +  "<br>Phone No.: " + document.getElementById("form_phone").value
-            +  "<br>Message: " + document.getElementById("form_message").value
-    }).then(
-      message => alert("Successfully sent contact enquiry.")
-    );
+async function send_email() {
+  Email.send({
+          SecureToken : "846303c5-e9f9-4670-ad88-556873b12df2",
+          To : "ninad.deshpande46@gmail.com",
+          From : "qrofeus@gmail.com",
+          Subject : "New contact form enquiry",
+          Body : "Name: " + document.getElementById("form_name").value
+              +  "<br>Mail Id: " + document.getElementById("form_mail").value
+              +  "<br>Phone No.: " + document.getElementById("form_phone").value
+              +  "<br>Message: " + document.getElementById("form_message").value
+      })
+      .then(
+        alert(`Successfully sent contact enquiry`)
+      )
 }
