f845950204401e9dc44abe5b1f176061b93e32ab
Qrofeus <ninad9021@gmail.com>
2024-02-07 10:41:49 -0500

    contact_form and navigation script update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,15 +1,4 @@
-// Navigation scroll shadow
-const header = document.querySelector("header");
-const content = document.createElement("div");
 
-content.setAttribute("data-scroll-interact", "");
-header.before(content);
-
-const content_observer = new IntersectionObserver((entries) => {
-    header.classList.toggle("nav_scrolled", !entries[0].isIntersecting);
-});
-
-content_observer.observe(content);
 
 // =====================================================================
 // Mobile Hamburger Menu
